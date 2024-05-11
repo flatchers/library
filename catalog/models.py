80f44c0d37726d3ed73db25b6c117b6f7da7d7e2
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-11 13:29:36 +0300

    added Book model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=255, choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=None, decimal_places=2)
