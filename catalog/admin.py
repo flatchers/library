da18a5736510aae5ba78093dc98a54f832e2eec5
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-13 15:31:43 +0300

    changes BookAdmin on advance lvl

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from catalog.models import Book, Borrowing, Payment
 
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "cover", "inventory", "daily_fee"]
+    list_filter = ["title", "author"]
+    search_fields = ["title", ]
+
+
 admin.site.register(Borrowing)
 admin.site.register(Payment)
