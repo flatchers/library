cea00d973c4e3ef8e57bfbf7a67953bac5d2bf75
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-13 14:03:57 +0300

    added username

--- user/serializers.py
+++ user/serializers.py
@@ -1,18 +1,20 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import AuthUser
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("id", "is_staff")
+        model = get_user_model()
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        return get_user_model().objects.create(**validated_data)
+        return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
