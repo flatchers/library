62f249530a398f7f4bb8330cb19f1a26b2e3518e
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-13 09:20:37 +0300

    changed UserSerializer -> fields

--- user/serializers.py
+++ user/serializers.py
@@ -7,7 +7,7 @@ from user.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "email", "first_name", "last_name", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
