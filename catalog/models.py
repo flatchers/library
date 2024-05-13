36bf25ab1e0d72669a9132e359b97404017c9626
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-13 16:57:46 +0300

    fixed and changes

--- catalog/models.py
+++ catalog/models.py
@@ -27,6 +27,9 @@ class Borrowing(models.Model):
     book_id = models.ManyToManyField(Book)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.borrow_date
+
 
 class Payment(models.Model):
 
@@ -40,6 +43,6 @@ class Payment(models.Model):
     status = models.CharField(max_length=255, choices=Status.choices)
     type = models.CharField(max_length=255, choices=Type.choices)
     borrowing_id = models.ForeignKey(Borrowing, related_name="types", on_delete=models.CASCADE)
-    session_url = models.URLField()
+    session_url = models.URLField(max_length=200)
     session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     money_to_pay = models.ManyToManyField(Borrowing)
