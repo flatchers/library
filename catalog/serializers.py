0e21f3883c3ba9309b2fe1b8a7da90538d906339
Heorhii Nadiradze <swain.volt@gmail.com>
2024-05-14 09:46:50 +0300

    added PaymentSerializer for Payment model

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from catalog.models import Book, Borrowing
+from catalog.models import Book, Borrowing, Payment
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -13,3 +13,9 @@ class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "borrow_date", "expected_return", "actual_return")
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ("id", "status", "type", "borrowing_id", "session_url", "session_id", "money_to_pay")
