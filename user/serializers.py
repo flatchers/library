f2f69f3bd19c3ab5d7cefef06aad8b5aa6f6c7c8
Heorhii Nadiradze <swain.volt@gmail.com>
2024-08-01 13:47:31 +0300

    changed registration and authentication from username to email

--- user/serializers.py
+++ user/serializers.py
@@ -9,7 +9,7 @@ from user.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff", "is_active")
+        fields = ("id", "email", "first_name", "last_name", "password", "is_staff", "is_active")
         read_only_fields = ("id", "is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
